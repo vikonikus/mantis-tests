87019e5dd73869febd6404e08cf64d61a1a21f04
konstantinovvn <v.konstantinov@haulmont.com>
2025-09-12 17:45:12 +0400

    Техправки на рабкомпе

--- src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -1,6 +1,17 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HttpSession {
